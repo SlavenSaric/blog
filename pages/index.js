935964cd8d56f06a3068c0878311a0e5a315257e
SlavenSaric <slavendownload@gmail.com>
2023-07-05 13:51:55 +0200

    featured posts with dummy data  added

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,40 @@
+import FeaturedPosts from "../components/home-page/Featured"
 import Hero from "../components/home-page/Hero"
+
+const DUMMY_POSTS = [
+    {
+        slug: 'getting-started-with-nextjs', 
+        title: 'Getting Started with NextJS', 
+        image: 'getting-started-nextjs.png', 
+        excerpt: 'NextJS is a React Framework for production - it makes building fullstackt apps and sites easier', 
+        date: '2022-02-12',
+    },
+    {
+        slug: 'getting-started-with-nextjs2', 
+        title: 'Getting Started with NextJS', 
+        image: 'getting-started-nextjs.png', 
+        excerpt: 'NextJS is a React Framework for production - it makes building fullstackt apps and sites easier', 
+        date: '2022-02-12',
+    },
+    {
+        slug: 'getting-started-with-nextjs3', 
+        title: 'Getting Started with NextJS', 
+        image: 'getting-started-nextjs.png', 
+        excerpt: 'NextJS is a React Framework for production - it makes building fullstackt apps and sites easier', 
+        date: '2022-02-12',
+    },
+    {
+        slug: 'getting-started-with-nextjs4', 
+        title: 'Getting Started with NextJS', 
+        image: 'getting-started-nextjs.png', 
+        excerpt: 'NextJS is a React Framework for production - it makes building fullstackt apps and sites easier', 
+        date: '2022-02-12',
+    },
+]
 
 export default function HomePage(){
     return <>
         <Hero />
+        <FeaturedPosts posts={DUMMY_POSTS}/>
     </>
 }
