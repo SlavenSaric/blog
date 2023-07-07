2e3922864f131837b25d16d949623cec24780edb
SlavenSaric <slavendownload@gmail.com>
2023-07-07 13:11:33 +0200

    detail edited

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,44 +2,15 @@ import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
-import { Prism } from "react-syntax-highlighter";
-import { atomOneDark } from "react-syntax-highlighter/dist/cjs/styles/hljs";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { dark } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
 
 export default function PostContent(props) {
   const { post } = props;
 
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
-  const costumRenderers = {
-    // image(image) {
-    //   return (
-    //     <Image
-    //       src={`images/posts/${post.slug}/${image.url}`}
-    //       alt={image.src}
-    //       width={600}
-    //       height={300}
-    //     />
-    //   );
-    // },
-    paragraph(paragraph) {
-      const { node } = paragraph;
-      if (node.children[0] === "image") {
-        const image = node.children[0];
-
-        return (
-          <div className={classes.image}>
-            <Image
-              src={`images/posts/${post.slug}/${image.url}`}
-              alt={image.src}
-              width={600}
-              height={300}
-            />
-          </div>
-        );
-      }
-      return <h1>{paragraph.children}</h1>
-    },
-  };
+ 
 
   return (
     <article className={classes.content}>
@@ -52,7 +23,7 @@ export default function PostContent(props) {
             code(code){
               const {lang, children} = code
               
-              return <Prism language={lang} style={atomOneDark}>{children}</Prism>
+              return <SyntaxHighlighter language={`${lang}`} style={dark}>{children}</SyntaxHighlighter>
             }
             
         }}>
